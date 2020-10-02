48c2b5e2d3729817cddeb37629ba31a6ff744391
ATQQ <engineerzjl@foxmail.com>
2020-10-03 00:09:40 +0800

    feat: 自动引入siderBar脚本初测成功

--- docs/.vuepress/siders/technology.js
+++ docs/.vuepress/siders/technology.js
@@ -1,10 +1,55 @@
 const { NavSider } = require('./object')
-function learnSidebar(group, introduction) {
+const path = require('path')
+const fs = require('fs')
+const defaultConfig = {
+    collapsable: false,
+    sidebarDepth: 2
+}
+const config = {
+    root: 'technology',
+    children: [
+        ['learn', 'devlearn', '开发教程']
+    ],
+}
+
+function getChildren(p, introduction) {
+    const files = fs.readdirSync(p, { withFileTypes: true })
+    const children = files.filter(f => f.isFile()).map(file => {
+        const { name } = file
+        const basename = path.basename(name, '.md')
+        const title = fs.readFileSync(path.resolve(p, name), {
+            encoding: 'utf-8'
+        }).split('\n')[0].slice(1).trim()
+        if (basename === 'README') {
+            return ['', introduction]
+        } else {
+            return [basename, title]
+        }
+    })
+    return children
+}
+
+function loadConfig(cf) {
+    const { root, children } = cf
+    const navSidebar = new NavSider(root)
+    children.forEach(child => {
+        const [childSider, title, introduction] = child
+        const children = getChildren(path.resolve(__dirname, `../../${root}`, childSider), introduction)
+        navSidebar.addChildSider(childSider,[
+            {
+                ...defaultConfig,
+                title,
+                children
+            }
+        ])
+    })
+    return navSidebar
+}
+function learnSidebar(title, introduction) {
     return [
         {
-            title: group,
-            collapsable: false,
-            sidebarDepth: 2,
+            ...defaultConfig,
+            title,
             children: [
                 ['', introduction],
                 ['eslint-plugin', 'eslint插件开发教程'],
@@ -17,4 +62,4 @@ function learnSidebar(group, introduction) {
 
 const technology = new NavSider('technology')
 technology.addChildSider('learn', learnSidebar('devlearn', '开发教程'))
-module.exports = technology.getSiders()
+module.exports = loadConfig(config).getSiders()
