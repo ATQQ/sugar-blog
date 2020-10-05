82505cbcfdfeb444dbb121b96de56132aa97a67d
ATQQ <engineerzjl@foxmail.com>
2020-10-05 11:19:51 +0800

    feat: 更新vue响应式原理的剖析,更新配置文件，缩减配置的内容

--- docs/.vuepress/siders/computerBase.js
+++ docs/.vuepress/siders/computerBase.js
@@ -1,103 +1,26 @@
-// const { NavSider } = require('./index')
-// function offerSidebar(group, introduction) {
-//     return [
-//         {
-//             title: group,
-//             collapsable: false,
-//             sidebarDepth: 2,
-//             children: [
-//                 ['', introduction],
-//                 ['p50', '面试题50:第一个只出现一次的字符'],
-//                 ['p53-2', '面试题53II:0～n-1中缺失的数字'],
-//                 ['p54', '面试题54:二叉搜索树的第k大节点'],
-//                 ['p55', '面试题55:平衡二叉树'],
-//                 ['p55-1', '面试题55:二叉树的深度'],
-//                 ['p58', '面试题58:左旋转字符串']
-//             ]
-//         }
-//     ]
-// }
-// function osSidebar(group, introduction) {
-//     return [
-//         {
-//             title: group,
-//             collapsable: false,
-//             sidebarDepth: 2,
-//             children: [
-//                 ['', introduction],
-//                 ['process', '进程'],
-//                 ['thread', '线程'],
-//                 ['difprothr', '进程与线程的区别'],
-//                 ['communicate', '进程和线程怎么通信'],
-//                 ['deadlock', '死锁']
-//             ]
-//         }
-//     ]
-// }
-// function internetSidebar(group, introduction) {
-//     return [
-//         {
-//             title: group,
-//             collapsable: false,
-//             sidebarDepth: 2,
-//             children: [
-//                 ['', introduction],
-//                 ['udp', 'UDP协议'],
-//                 ['tcp', 'TCP协议'],
-//                 ['tcp-udp', 'TCP与UDP的区别'],
-//                 ['http', 'HTTP协议'],
-//                 ['clength', 'Content-Length']
-//             ]
-//         }
-//     ]
-// }
-// function designSidebar(group, introduction) {
-//     return [
-//         {
-//             title: group,
-//             collapsable: false,
-//             sidebarDepth: 2,
-//             children: [
-//                 ['', introduction],
-//                 ['factory', '工厂模式'],
-//                 ['abstractfactory', '抽象工厂模式'],
-//                 ['single', '单例模式']
-//             ]
-//         }
-//     ]
-// }
-
-// function algorithmSidebar(group, introduction) {
-//     return [
-//         {
-//             title: group,
-//             collapsable: false,
-//             sidebarDepth: 2,
-//             children: [
-//                 ['', introduction],
-//                 ['sort', '排序算法的稳定性'],
-//             ]
-//         }
-//     ]
-// }
-
-
-// const computerBase = new NavSider('computerBase')
-// computerBase.addChildSider('offer', offerSidebar('剑指Offer', '简介'))
-// computerBase.addChildSider('os', osSidebar('os', '操作系统'))
-// computerBase.addChildSider('Internet', internetSidebar('Internet', '计算机网络'))
-// computerBase.addChildSider('design', designSidebar('design Pattern', '设计模式'))
-// computerBase.addChildSider('algorithm', algorithmSidebar('algorithm and data', '算法与数据结构'))
-
-// module.exports = computerBase.getSiders()
-
 module.exports = {
     root: 'computerBase',
     children: [
         ['offer', '剑指Offer', '简介'],
-        ['os', 'OS', '操作系统'],
-        ['Internet', 'Internet', '计算机网络'],
-        ['design', 'Design Pattern', '设计模式'],
+        ['os', 'OS', '操作系统', [
+            ['process', '进程'],
+            ['thread', '线程'],
+            ['difprothr', '进程与线程的区别'],
+            ['communicate', '进程和线程怎么通信'],
+            ['deadlock', '死锁']
+        ]],
+        ['Internet', 'Internet', '计算机网络', [
+            ['udp', 'UDP协议'],
+            ['tcp', 'TCP协议'],
+            ['tcp-udp', 'TCP与UDP的区别'],
+            ['http', 'HTTP协议'],
+            ['clength', 'Content-Length']
+        ]],
+        ['design', 'Design Pattern', '设计模式', [
+            ['factory', '工厂模式'],
+            ['abstractfactory', '抽象工厂模式'],
+            ['single', '单例模式']
+        ]],
         ['algorithm', 'Algorithm And Data', '算法与数据结构'],
     ],
 }
