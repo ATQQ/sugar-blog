8a4c7103396728db8b8ebb226ba27c2cbe8d7f6b
sugar <engineerzjl@foxmail.com>
2022-03-30 19:35:23 +0800

    feat: 评论插件替换为giscus

--- docs/.vuepress/components/comment.vue
+++ docs/.vuepress/components/comment.vue
@@ -1,13 +1,10 @@
 <template>
-  <div
-    class="comment"
-    id="valine-comment"
-  >
+  <div class="comment" id="valine-comment">
     <!-- <div id="comments"></div>
     <span
       id="pathname"
       class="leancloud_visitors"
-      data-flag-title="Your Article Title"
+      flag-title="Your Article Title"
     >
       <em class="post-meta-item-text"
         >阅读量：</em
@@ -15,7 +12,23 @@
       <i
         class="leancloud-visitors-count"
       ></i>
-    </span> -->
+    </span>-->
+    <script src="https://giscus.app/client.js"
+        data-repo="ATQQ/sugar-blog"
+        data-repo-id="MDEwOlJlcG9zaXRvcnkyNDEyNDUyOTk="
+        data-category="Announcements"
+        data-category-id="DIC_kwDODmEcc84COVc6"
+        data-mapping="pathname"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="top"
+        data-theme="light"
+        data-lang="zh-CN"
+        crossorigin="anonymous"
+        async>
+</script>
+
+
   </div>
 </template>
 
@@ -23,11 +36,11 @@
 export default {
   name: 'comment',
   mounted() {
-    import('./../comment').then(
-      (res) => {
-        res.default.init();
-      }
-    );
+    // import('./../comment').then(
+    //   (res) => {
+    //     res.default.init();
+    //   }
+    // );
   },
 };
 </script>
