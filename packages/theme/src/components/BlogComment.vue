32aef881c1a39030463e5a264dd8dd01fb9b5495
sugar <engineerzjl@foxmail.com>
2023-01-03 23:12:23 +0800

    WIP: 评论区跳转逻辑

--- packages/theme/src/components/BlogComment.vue
+++ packages/theme/src/components/BlogComment.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="comment" v-if="show">
+  <div class="comment" v-if="show" id="giscus-comment">
     <component
       v-if="showComment"
       :is="'script'"
@@ -19,15 +19,29 @@
       async
     >
     </component>
+    <!-- TODO：跳转到评论区优化 -->
+    <el-affix
+      :class="{ hidden: !showCommnetAffix }"
+      position="bottom"
+      @change="handleVisibleChange"
+      :offset="40"
+    >
+      <el-button type="primary">Offset bottom 20px</el-button>
+    </el-affix>
   </div>
 </template>
 <script setup lang="ts">
 import { useDark } from '@vueuse/core'
 import { useRoute } from 'vitepress'
 import { computed, ref, watch } from 'vue'
+import { ElAffix, ElButton } from 'element-plus'
 import { useGiscusConfig } from '../composables/config/blog'
 import { Theme } from '../composables/config/index'
 
+const showCommnetAffix = ref(true)
+const handleVisibleChange = (v: boolean) => {
+  showCommnetAffix.value = v
+}
 const giscusConfig = useGiscusConfig()
 
 const commentConfig = computed<Partial<Theme.GiscusConfig>>(() => {
@@ -70,5 +84,10 @@ watch(
   width: 100%;
   text-align: center;
   padding: 40px 0;
+}
+
+.hidden {
+  opacity: 0;
+  pointer-events: none;
 }
 </style>
