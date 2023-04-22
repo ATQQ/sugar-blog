0976a5102c62a676902696911cb8c53fe93f0514
sugar <engineerzjl@foxmail.com>
2023-04-22 14:44:17 +0800

    feat: 首页卡片调整

--- packages/theme/src/components/BlogItem.vue
+++ packages/theme/src/components/BlogItem.vue
@@ -1,31 +1,40 @@
 <template>
   <a class="blog-item" :href="withBase(route)">
     <i class="pin" v-if="!!pin"></i>
-    <!-- 左侧信息  -->
-    <div class="info-part">
-      <!-- 标题 -->
-      <p class="title">{{ title }}</p>
-      <!-- 简短描述 -->
-      <p class="description" v-if="!!description">{{ description }}</p>
-      <div class="badge-list">
-        <span class="split" v-if="author">{{ author }}</span>
-        <span class="split">{{ showTime }}</span>
-        <span class="split" v-if="tag?.length">{{ tag.join(' · ') }}</span>
+    <!-- 标题 -->
+    <p class="title" v-if="inMobile">{{ title }}</p>
+    <div class="info-container">
+      <!-- 左侧信息 -->
+      <div class="info-part">
+        <!-- 标题 -->
+        <p class="title" v-if="!inMobile">{{ title }}</p>
+        <!-- 简短描述 -->
+        <p class="description" v-if="!!description">{{ description }}</p>
       </div>
+      <!-- 右侧封面图 -->
+      <div
+        v-if="cover"
+        class="cover-img"
+        :style="`background-image: url(${cover});`"
+      ></div>
     </div>
-    <div
-      v-if="cover"
-      class="cover-img"
-      :style="`background-image: url(${cover});`"
-    ></div>
+    <!-- 底部补充描述 -->
+    <div class="badge-list">
+      <span class="split" v-if="author">{{ author }}</span>
+      <span class="split">{{ showTime }}</span>
+      <span class="split" v-if="tag?.length">{{ tag.join(' · ') }}</span>
+    </div>
   </a>
 </template>
 
 <script lang="ts" setup>
 import { withBase } from 'vitepress'
 import { computed } from 'vue'
+import { useWindowSize } from '@vueuse/core'
 import { formatShowDate } from '../utils/index'
 
+const { width } = useWindowSize()
+const inMobile = computed(() => width.value <= 500)
 const props = defineProps<{
   route: string
   title: string
@@ -86,10 +95,15 @@ const showTime = computed(() => {
   background-color: rgba(var(--bg-gradient));
   cursor: pointer;
   display: flex;
-  align-items: center;
+  flex-direction: column;
   &:hover {
     box-shadow: var(--box-shadow-hover);
   }
+}
+.info-container {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
 }
 
 .info-part {
