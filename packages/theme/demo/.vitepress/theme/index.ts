1b5aafed4c606c546ce8856425fe98498704e20a
sugar <engineerzjl@foxmail.com>
2023-07-21 21:42:43 +0800

    chore: 移除Theme.HomeConfig 的click

--- packages/theme/demo/.vitepress/theme/index.ts
+++ packages/theme/demo/.vitepress/theme/index.ts
@@ -1,26 +1,8 @@
-import BlogTheme, { Theme } from '@sugarat/theme'
-import { ElMessage } from 'element-plus'
-import { h } from 'vue'
+import BlogTheme from '@sugarat/theme'
 import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
-
-const AIWords = [
-  '无论多么沉重的负担，也不要忘记微笑；无论多么漫长的路程，也不要忘记坚持',
-  '生活的真谛不在繁华，而在于淡泊'
-]
-
-const homeProps: Theme.HomeConfig = {
-  handleChangeSlogan() {
-    ElMessage.success({
-      message: '支持点击修改内容',
-      duration: 888
-    })
-    return AIWords[Math.floor(Math.random() * AIWords.length)]
-  }
-}
 
 export default {
   ...BlogTheme,
-  Layout: h(BlogTheme.Layout as any, homeProps),
   enhanceApp(ctx: any) {
     BlogTheme?.enhanceApp?.(ctx)
     enhanceAppWithTabs(ctx.app)
