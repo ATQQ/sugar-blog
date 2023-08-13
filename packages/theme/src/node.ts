db998276650b9f4e8417d8449c1ee637657b681a
sugar <engineerzjl@foxmail.com>
2023-08-13 16:47:05 +0800

    feat: 拆分Vite插件激活逻辑

--- packages/theme/src/node.ts
+++ packages/theme/src/node.ts
@@ -1,8 +1,6 @@
 /* eslint-disable global-require */
 /* eslint-disable prefer-rest-params */
-import { execSync } from 'child_process'
-import path from 'path'
-import type { SiteConfig, UserConfig } from 'vitepress'
+import type { UserConfig } from 'vitepress'
 import type { Theme } from './composables/config/index'
 import {
   getMarkdownPlugins,
@@ -11,6 +9,7 @@ import {
   supportRunExtendsPlugin
 } from './utils/node/mdPlugins'
 import { getArticles, patchDefaultThemeSideBar } from './utils/node/theme'
+import { getVitePlugins, registerVitePlugins } from './utils/node/vitePlugins'
 
 /**
  * 获取主题的配置
@@ -19,79 +18,17 @@ import { getArticles, patchDefaultThemeSideBar } from './utils/node/theme'
 export function getThemeConfig(cfg?: Partial<Theme.BlogConfig>) {
   // 文章数据
   const pagesData = getArticles(cfg)
-  const extraConfig: any = {}
+  const extraVPConfig: any = {}
 
-  if (
-    cfg?.search === 'pagefind' ||
-    (cfg?.search instanceof Object && cfg.search.mode === 'pagefind')
-  ) {
-    let resolveConfig: any
-    extraConfig.vite = {
-      plugins: [
-        {
-          name: '@sugarar/theme-plugin-pagefind',
-          enforce: 'pre',
-          configResolved(config: any) {
-            if (resolveConfig) {
-              return
-            }
-            resolveConfig = config
-
-            const vitepressConfig: SiteConfig = config.vitepress
-            if (!vitepressConfig) {
-              return
-            }
-
-            // 添加 自定义 vitepress 的钩子
-            const selfBuildEnd = vitepressConfig.buildEnd
-            vitepressConfig.buildEnd = (siteConfig: any) => {
-              // 调用自己的
-              selfBuildEnd?.(siteConfig)
-              // 调用pagefind
-              const ignore: string[] = [
-                // 侧边栏内容
-                'div.aside',
-                // 标题锚点
-                'a.header-anchor'
-              ]
-              const { log } = console
-              log()
-              log('=== pagefind: https://pagefind.app/ ===')
-              let command = `npx pagefind --source ${path.join(
-                process.argv.slice(2)?.[1] || '.',
-                '.vitepress/dist'
-              )}`
-
-              if (ignore.length) {
-                command += ` --exclude-selectors "${ignore.join(', ')}"`
-              }
-
-              log(command)
-              log()
-              execSync(command, {
-                stdio: 'inherit'
-              })
-            }
-          },
-          // 添加检索的内容标识
-          transform(code: string, id: string) {
-            if (id.endsWith('theme-default/Layout.vue')) {
-              return code.replace(
-                '<VPContent>',
-                '<VPContent data-pagefind-body>'
-              )
-            }
-            return code
-          }
-        }
-      ]
-    }
-  }
+  // 获取要加载的vite插件
+  const vitePlugins = getVitePlugins(cfg)
+  // 注册Vite插件
+  registerVitePlugins(extraVPConfig, vitePlugins)
 
   // 获取要加载的markdown插件
   const markdownPlugin = getMarkdownPlugins(cfg)
   // 注册markdown插件
-  registerMdPlugins(extraConfig, markdownPlugin)
+  registerMdPlugins(extraVPConfig, markdownPlugin)
 
   // 用于自定义sidebar卡片slot
   const extraSidebar = patchDefaultThemeSideBar(cfg)
@@ -104,7 +41,7 @@ export function getThemeConfig(cfg?: Partial<Theme.BlogConfig>) {
       },
       ...extraSidebar
     },
-    ...extraConfig
+    ...extraVPConfig
   }
 }
 
