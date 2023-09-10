f66268ecc55f4e00ae42339ed192f05316312352
sugar <engineerzjl@foxmail.com>
2023-09-10 18:11:48 +0800

    chore: 移除过期的方法

--- packages/theme/src/composables/config/blog.ts
+++ packages/theme/src/composables/config/blog.ts
@@ -21,22 +21,13 @@ const configSymbol: InjectionKey<Ref<Theme.Config>> = Symbol('theme-config')
 const activeTagSymbol: InjectionKey<Ref<Theme.activeTag>> = Symbol('active-tag')
 
 const currentPageNum: InjectionKey<Ref<number>> = Symbol('home-page-num')
-const homeConfigSymbol: InjectionKey<Theme.HomeConfig> = Symbol('home-config')
 
 const userWorks: InjectionKey<Ref<Theme.UserWorks>> = Symbol('user-works')
 
 export function withConfigProvider(App: Component) {
   return defineComponent({
     name: 'ConfigProvider',
-    props: {
-      handleChangeSlogan: {
-        type: Function,
-        required: false
-      }
-    },
-    setup(props, { slots }) {
-      provide(homeConfigSymbol, props as Theme.HomeConfig)
-
+    setup(_, { slots }) {
       const { theme } = useData()
       const config = computed(() => resolveConfig(theme.value))
       provide(configSymbol, config)
@@ -76,9 +67,6 @@ export function useBlogConfig() {
 
 export function useBlogThemeMode() {
   return inject(configSymbol)!.value?.blog?.blog ?? true
-}
-export function useHomeConfig() {
-  return inject(homeConfigSymbol)!
 }
 
 export function useGiscusConfig() {
