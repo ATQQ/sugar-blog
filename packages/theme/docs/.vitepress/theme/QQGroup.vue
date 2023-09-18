404c7cf9fdc9196b9cd6f607a9a86b39e66e6d2f
sugar <engineerzjl@foxmail.com>
2023-09-18 22:20:15 +0800

    feat: 主题文档首尾添加QQ交流群链接

---
+++ packages/theme/docs/.vitepress/theme/QQGroup.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="qq-group">
+        <p>🎉 欢迎加入 <a href="https://qm.qq.com/q/rqMyNvGyvS" target="_blank" rel="noopener noreferrer">主题QQ交流群：681489336</a></p>
+        <p v-if="showImg">
+            <img src="https://img.cdn.sugarat.top/mdImg/MTY5NTA0NTAzMjAzMQ==695045032031" alt="QQ交流群">
+        </p>
+    </div>
+</template>
+<script setup>
+import { defineProps } from 'vue'
+const props = defineProps({
+    showImg: {
+        type: Boolean,
+        default: false
+    }
+})
+</script>
+<style scoped>
+.qq-group {
+    text-align: center;
+}
+
+.qq-group img {
+    width: 200px;
+    display: inline-block;
+    cursor: pointer;
+}
+
+.qq-group p {
+    padding: 16px 0;
+}
+
+.qq-group a {
+    background: linear-gradient(90deg, #58e3f5, #dabcfc);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    padding-bottom: 4px;
+    border-bottom: 1px dashed #58e3f5;
+    font-weight: bolder;
+}
+</style>
--- packages/theme/docs/.vitepress/theme/index.ts
+++ packages/theme/docs/.vitepress/theme/index.ts
@@ -2,9 +2,15 @@ import BlogTheme from '@sugarat/theme'
 import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
 import './theme.var.css'
 import ChangeThemeDemo from './ChangeThemeDemo.vue'
+import QQGroup from './QQGroup.vue'
+import { h } from 'vue'
 
 export default {
   ...BlogTheme,
+  Layout: h(BlogTheme.Layout, undefined, {
+    'doc-before': h(QQGroup),
+    'doc-after': h(QQGroup, { showImg: true })
+  }),
   enhanceApp(ctx: any) {
     BlogTheme?.enhanceApp?.(ctx)
     enhanceAppWithTabs(ctx.app)
