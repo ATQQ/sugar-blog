becce19c66393089e0e224bbb7acd62372aefed5
sugar <engineerzjl@foxmail.com>
2024-01-31 22:09:47 +0800

    WIP: add swiper

--- packages/theme/src/components/BlogFriendLink.vue
+++ packages/theme/src/components/BlogFriendLink.vue
@@ -1,7 +1,8 @@
 <script lang="ts" setup>
 import { ElAvatar } from 'element-plus'
-import { useDark } from '@vueuse/core'
-import { computed } from 'vue'
+import { useDark, useIntervalFn } from '@vueuse/core'
+import { computed, onMounted, onUnmounted, ref } from 'vue'
+import type Swiper from 'swiper'
 import { useBlogConfig } from '../composables/config/blog'
 import { getImageUrl, shuffleArray } from '../utils/client'
 import type { Theme } from '../'
@@ -26,7 +27,7 @@ const limit = computed(() => {
 
 const scrollSpeed = computed(() => {
   const { scrollSpeed } = friendConfig.value
-  return (!scrollSpeed || scrollSpeed <= 0) ? (friendConfig.value.list.length - 1) * (friendConfig.value.list.length / limit.value) * 1000 : scrollSpeed
+  return scrollSpeed || 1000
 })
 
 const openScroll = computed(() => {
@@ -58,10 +59,10 @@ const friendList = computed(() => {
       alt
     }
   })
-  return openScroll.value ? [...list, ...list] : list
+  return list
 })
 
-const cardHeight = 83
+const cardHeight = 80
 const scrollWrapperHeight = computed(() => {
   return openScroll.value ? limit.value * cardHeight : 0
 })
@@ -70,6 +71,28 @@ const containerHeight = computed(() => {
 })
 const scrollTop = computed(() => {
   return `-${scrollWrapperHeight.value * 2}px`
+})
+
+const swiper = ref<Swiper>()
+const { resume, pause } = useIntervalFn(() => {
+  // swiper.value?.slideNext()
+}, scrollSpeed.value)
+
+onMounted(() => {
+  pause()
+  if (openScroll.value) {
+    // eslint-disable-next-line no-new
+    // swiper.value = new Swiper('.scroll-wrapper', {
+    //   direction: 'vertical',
+    //   slidesPerView: limit.value,
+    //   loop: true,
+    // })
+    resume()
+  }
+})
+
+onUnmounted(() => {
+  pause()
 })
 </script>
 
@@ -102,14 +125,8 @@ const scrollTop = computed(() => {
         height: containerHeight,
       }"
     >
-      <ol
-        class="friend-list" :style="{
-          animationPlayState: openScroll ? 'running' : 'paused',
-          animationDuration: `${scrollSpeed / 1000}s`,
-        }
-        "
-      >
-        <li v-for="(v, idx) in friendList" :key="idx">
+      <ol class="friend-list swiper-wrapper">
+        <li v-for=" (v, idx) in friendList" :key="idx" class="swiper-slide">
           <a :href="v.url" target="_blank">
             <ElAvatar :size="50" :src="v.avatar" :alt="v.alt" />
             <div>
@@ -159,15 +176,6 @@ const scrollTop = computed(() => {
   flex-direction: column;
 }
 
-@keyframes scrollList {
-  0% {
-    top: 0;
-  }
-  100% {
-    top: v-bind(scrollTop);
-  }
-}
-
 .scroll-wrapper {
   overflow: hidden;
   position: relative;
@@ -178,22 +186,17 @@ const scrollTop = computed(() => {
   flex-direction: column;
   list-style: none;
   margin: 0;
-  padding: 0 10px 0 0px;
+  padding: 10px 10px 0 0px;
   width: 100%;
 
   position: relative;
   width: 100%;
-  animation-name: scrollList;
-  animation-timing-function:linear;
-  animation-iteration-count:infinite;
-
-  &:hover {
-    animation-play-state: paused !important;
-  }
 
   li {
-    padding: 6px;
-    margin-top: 10px;
+    box-sizing: border-box;
+    padding: 0 5px;
+    height: 80px;
+    margin-bottom: 10px;
 
     .el-avatar {
       min-width: 50px;
@@ -201,6 +204,7 @@ const scrollTop = computed(() => {
 
     a {
       display: flex;
+      align-items: center;
     }
 
     div {
