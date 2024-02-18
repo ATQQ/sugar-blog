ae455e8d16a31d7b832fbb5eed881453f89682c6
sugar <engineerzjl@foxmail.com>
2024-02-18 16:02:12 +0800

    chore: 移除 documate

--- packages/theme/docs/.vitepress/theme/index.ts
+++ packages/theme/docs/.vitepress/theme/index.ts
@@ -1,19 +1,14 @@
 import BlogTheme from '@sugarat/theme'
 import './theme.var.css'
 import { h } from 'vue'
-import Documate from '@documate/vue'
 import ChangeThemeDemo from './ChangeThemeDemo.vue'
 import JoinGroup from './JoinGroup.vue'
-import '@documate/vue/dist/style.css'
 
 export default {
   ...BlogTheme,
   Layout: h(BlogTheme.Layout, undefined, {
     'doc-before': () => h(JoinGroup),
     'doc-after': () => h(JoinGroup, { showImg: true }),
-    'nav-bar-content-before': () => h(Documate, {
-      endpoint: 'https://sfueei2j1g.us.aircode.run/ask',
-    }),
   }),
   enhanceApp(ctx: any) {
     BlogTheme?.enhanceApp?.(ctx)
