7da39077fdf4c539004f2f1fc2c9c8ffbb840073
sugar <engineerzjl@foxmail.com>
2024-05-06 13:36:40 +0800

    fix: generate title error

--- packages/vitepress-plugin-rss/src/utils/index.ts
+++ packages/vitepress-plugin-rss/src/utils/index.ts
@@ -66,3 +66,26 @@ export function getFileBirthTime(url: string) {
 
   return date
 }
+
+export function getTextSummary(text: string, count = 100) {
+  return (
+    text
+      // 首个标题
+      ?.replace(/^#+\s+.*/, '')
+      // 除去标题
+      ?.replace(/#/g, '')
+      // 除去图片
+      ?.replace(/!\[.*?\]\(.*?\)/g, '')
+      // 除去链接
+      ?.replace(/\[(.*?)\]\(.*?\)/g, '$1')
+      // 除去加粗
+      ?.replace(/\*\*(.*?)\*\*/g, '$1')
+      ?.split('\n')
+      ?.filter(v => !!v)
+      ?.join('\n')
+      ?.replace(/>(.*)/, '')
+      ?.replace(/</g, '&lt;').replace(/>/g, '&gt;')
+      ?.trim()
+      ?.slice(0, count)
+  )
+}
