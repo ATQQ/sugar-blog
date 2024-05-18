f9e671199da7751d954015bdd57b5d2109122ce4
sugar <engineerzjl@foxmail.com>
2024-05-18 16:49:14 +0800

    feat: 移植样式，对齐local-search

--- packages/vitepress-plugin-pagefind/src/Search.vue
+++ packages/vitepress-plugin-pagefind/src/Search.vue
@@ -3,13 +3,13 @@
 import { computed, nextTick, onMounted, ref, watch } from 'vue'
 import { Command } from 'vue-command-palette'
 import { useData, useRoute, useRouter, withBase } from 'vitepress'
-import { useMagicKeys } from '@vueuse/core'
+import { useLocalStorage, useMagicKeys } from '@vueuse/core'
 import { searchConfig as _searchConfig, docs } from 'virtual:pagefind'
 import { formatDate } from './utils'
 import LogoPagefind from './LogoPagefind.vue'
 import type { SearchConfig } from './type'
 
-const searchResult = ref<{ route: string;meta: Record<string, any> }[]>([])
+const searchResult = ref<{ route: string; meta: Record<string, any> }[]>([])
 
 const searchConfig: SearchConfig = _searchConfig
 
@@ -273,6 +273,20 @@ watch(
     }
   }
 )
+
+// 清空搜索关键词
+const searchInput = ref<HTMLInputElement>()
+function handleClearSearch() {
+  searchWords.value = ''
+  nextTick(() => {
+    searchInput.value.$el.value = ''
+  })
+}
+
+const showDetail = useLocalStorage('pagefind-search-showDetail', false)
+function handleToggleDetail() {
+  showDetail.value = !showDetail.value
+}
 </script>
 
 <template>
@@ -282,11 +296,7 @@ watch(
         <svg width="14" height="14" viewBox="0 0 20 20">
           <path
             d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z"
-            stroke="currentColor"
-            fill="none"
-            fill-rule="evenodd"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"
           />
         </svg>
       </span>
@@ -298,10 +308,23 @@ watch(
     <ClientOnly>
       <Command.Dialog :visible="searchModal" theme="algolia">
         <template #header>
-          <Command.Input
-            v-model:value="searchWords"
-            :placeholder="finalSearchConfig?.placeholder || 'Search Docs'"
-          />
+          <div class="search-bar">
+            <Command.Input
+              ref="searchInput" v-model:value="searchWords"
+              :placeholder="finalSearchConfig?.placeholder || 'Search Docs'"
+            />
+            <div class="search-actions">
+              <button :class="{ active: showDetail }" class="toggle-layout-button" type="button" title="Display detailed list" @click="handleToggleDetail">
+                <span class="vpi-layout-list local-search-icon" />
+              </button>
+              <button
+                :disabled="!searchWords" class="clear-button" type="reset" title="Reset search"
+                @click="handleClearSearch"
+              >
+                <span class="vpi-delete local-search-icon" />
+              </button>
+            </div>
+          </div>
         </template>
         <template #body>
           <div class="search-dialog">
@@ -311,9 +334,7 @@ watch(
               </Command.Empty>
               <Command.Group v-else :heading="headingText">
                 <Command.Item
-                  v-for="item in showSearchResult"
-                  :key="item.route"
-                  :data-value="item.route"
+                  v-for="item in showSearchResult" :key="item.route" :data-value="item.route"
                   @select="handleSelect"
                 >
                   <div class="link">
@@ -331,11 +352,7 @@ watch(
         </template>
         <template v-if="searchResult.length" #footer>
           <div class="command-palette-logo">
-            <a
-              href="https://github.com/cloudcannon/pagefind"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <a href="https://github.com/cloudcannon/pagefind" target="_blank" rel="noopener noreferrer">
               <span class="command-palette-Label">Search by</span>
               <LogoPagefind style="width: 77px" />
             </a>
@@ -344,51 +361,44 @@ watch(
             <li>
               <kbd class="command-palette-commands-key"><svg width="15" height="15" aria-label="Enter key" role="img">
                 <g
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                   stroke-width="1.2"
                 >
-                  <path
-                    d="M12 3.53088v3c0 1-1 2-2 2H4M7 11.53088l-3-3 3-3"
-                  />
-                </g></svg></kbd><span class="command-palette-Label">to select</span>
+                  <path d="M12 3.53088v3c0 1-1 2-2 2H4M7 11.53088l-3-3 3-3" />
+                </g>
+              </svg></kbd><span class="command-palette-Label">to select</span>
             </li>
             <li>
               <kbd class="command-palette-commands-key"><svg width="15" height="15" aria-label="Arrow down" role="img">
                 <g
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                   stroke-width="1.2"
                 >
                   <path d="M7.5 3.5v8M10.5 8.5l-3 3-3-3" />
-                </g></svg></kbd><kbd class="command-palette-commands-key"><svg width="15" height="15" aria-label="Arrow up" role="img">
+                </g>
+              </svg></kbd><kbd class="command-palette-commands-key"><svg
+                width="15" height="15" aria-label="Arrow up"
+                role="img"
+              >
                 <g
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                   stroke-width="1.2"
                 >
                   <path d="M7.5 11.5v-8M10.5 6.5l-3-3-3 3" />
-                </g></svg></kbd><span class="command-palette-Label">to navigate</span>
+                </g>
+              </svg></kbd><span class="command-palette-Label">to navigate</span>
             </li>
             <li>
               <kbd class="command-palette-commands-key"><svg width="15" height="15" aria-label="Escape key" role="img">
                 <g
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                   stroke-width="1.2"
                 >
                   <path
                     d="M13.6167 8.936c-.1065.3583-.6883.962-1.4875.962-.7993 0-1.653-.9165-1.653-2.1258v-.5678c0-1.2548.7896-2.1016 1.653-2.1016.8634 0 1.3601.4778 1.4875 1.0724M9 6c-.1352-.4735-.7506-.9219-1.46-.8972-.7092.0246-1.344.57-1.344 1.2166s.4198.8812 1.3445.9805C8.465 7.3992 8.968 7.9337 9 8.5c.032.5663-.454 1.398-1.4595 1.398C6.6593 9.898 6 9 5.963 8.4851m-1.4748.5368c-.2635.5941-.8099.876-1.5443.876s-1.7073-.6248-1.7073-2.204v-.4603c0-1.0416.721-2.131 1.7073-2.131.9864 0 1.6425 1.031 1.5443 2.2492h-2.956"
                   />
-                </g></svg></kbd><span class="command-palette-Label">to close</span>
+                </g>
+              </svg></kbd><span class="command-palette-Label">to close</span>
             </li>
           </ul>
         </template>
@@ -403,6 +413,7 @@ watch(
   display: flex;
   padding-left: 32px;
 }
+
 .blog-search .nav-search-btn-wait {
   cursor: pointer;
   display: flex;
@@ -414,14 +425,17 @@ watch(
   border-radius: 6px;
   transition: .2s border;
 }
+
 .blog-search .nav-search-btn-wait .metaKey {
   margin-left: 10px;
   font-size: 12px;
 }
+
 .blog-search .nav-search-btn-wait:hover {
   border: 1px solid var(--vp-c-brand-1);
   border-radius: 6px;
 }
+
 .blog-search .nav-search-btn-wait .search-tip {
   color: #909399;
   font-size: 12px;
@@ -429,15 +443,52 @@ watch(
 }
 
 @media screen and (max-width: 759px) {
-  .metaKey{
+  .metaKey {
     display: none;
   }
-  .search-tip{
+
+  .search-tip {
     display: none;
   }
-  .blog-search{
+
+  .blog-search {
     flex: 0;
   }
+}
+
+.search-bar {
+  display: flex;
+  cursor: text;
+  align-items: center;
+  border-radius: 4px;
+  border: 1px solid var(--vcp-c-brand);
+}
+
+.search-bar input {
+  width: 100%;
+}
+
+.search-bar .search-actions {
+  display: flex;
+  gap: 4px;
+  padding-right: 12px;
+}
+
+.search-actions button {
+  padding: 8px;
+}
+
+.local-search-icon {
+  display: block;
+  font-size: 18px;
+}
+
+.search-actions button.clear-button:disabled {
+  opacity: 0.37;
+}
+
+.search-actions button:not([disabled]):hover,.search-actions button.active:not([disabled]) {
+  color: var(--vp-c-brand-1);
 }
 </style>
 
