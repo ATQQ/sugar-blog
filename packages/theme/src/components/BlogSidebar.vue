c2d7f296475076e796dc347a9d4cc5bbfe7fe7e5
sugar <engineerzjl@foxmail.com>
2024-10-15 20:02:46 +0800

    WIP: i8n

--- packages/theme/src/components/BlogSidebar.vue
+++ packages/theme/src/components/BlogSidebar.vue
@@ -1,17 +1,17 @@
 <script lang="ts" setup>
 import { computed } from 'vue'
-import { useBlogConfig } from '../composables/config/blog'
+import { useRecommendConfig, useShowRecommend } from '../composables/config/blog'
 import BlogRecommendArticle from './BlogRecommendArticle.vue'
 
-const { recommend: _recommend } = useBlogConfig()
-
+const recommend = useRecommendConfig()
+const show = useShowRecommend()
 const sidebarStyle = computed(() =>
-  _recommend && _recommend?.style ? _recommend.style : 'sidebar'
+  recommend.value?.style || 'sidebar'
 )
 </script>
 
 <template>
-  <div v-if="_recommend !== false" class="sidebar" :class="{ card: sidebarStyle === 'card' }" data-pagefind-ignore="all">
+  <div v-if="show" class="sidebar" :class="{ card: sidebarStyle === 'card' }" data-pagefind-ignore="all">
     <BlogRecommendArticle />
   </div>
 </template>
