65630919cb08487f6f09d19182cf13f9d80268f6
sugar <engineerzjl@foxmail.com>
2026-06-07 11:00:32 +0800

    ci: 新增kitecd配置并更新部署脚本为kite推送方式
    为theme和blogpress包新增kite配置文件，更新部署脚本为构建后通过kite推送的流程，注释原有的远程服务器部署逻辑

--- deploy-theme.mjs
+++ deploy-theme.mjs
@@ -1,24 +1,28 @@
 #!/usr/bin/env zx
 
-// user config
-const originName = 'theme'
-
-// not care
-const compressPkgName = `${originName}.tar.gz`
-const user = 'root'
-const origin = 'sugarat.top'
-const fullOrigin = `${originName}.${origin}`
-const baseServerDir = '/www/wwwroot'
-const destDir = ''
-
 await $`pnpm buildTheme`
 
-await $`echo ==🔧 压缩dist ==`
-await $`cd packages/theme/docs/.vitepress && tar -zvcf ${compressPkgName} dist && rm -rf dist && mv ${compressPkgName} ./../../../../`
+await $`cd packages/theme && kite push`
 
-await $`echo ==🚀 上传到服务器 ==`
-await $`scp ${compressPkgName} ${user}@${fullOrigin}:./`
-await $`rm -rf ${compressPkgName}`
+// user config
+// const originName = 'theme'
 
-await $`echo ==✅ 部署代码 ==`
-await $`ssh -p22 ${user}@${fullOrigin} "tar -xf ${compressPkgName} -C ${baseServerDir}/${fullOrigin}/${destDir}"`
+// // not care
+// const compressPkgName = `${originName}.tar.gz`
+// const user = 'root'
+// const origin = 'sugarat.top'
+// const fullOrigin = `${originName}.${origin}`
+// const baseServerDir = '/www/wwwroot'
+// const destDir = ''
+
+// await $`pnpm buildTheme`
+
+// await $`echo ==🔧 压缩dist ==`
+// await $`cd packages/theme/docs/.vitepress && tar -zvcf ${compressPkgName} dist && rm -rf dist && mv ${compressPkgName} ./../../../../`
+
+// await $`echo ==🚀 上传到服务器 ==`
+// await $`scp ${compressPkgName} ${user}@${fullOrigin}:./`
+// await $`rm -rf ${compressPkgName}`
+
+// await $`echo ==✅ 部署代码 ==`
+// await $`ssh -p22 ${user}@${fullOrigin} "tar -xf ${compressPkgName} -C ${baseServerDir}/${fullOrigin}/${destDir}"`
--- deploy.mjs
+++ deploy.mjs
@@ -1,27 +1,31 @@
 #!/usr/bin/env zx
 
+await $`pnpm buildTheme`
+
+await $`cd packages/blogpress && kite push`
+
 // user config
-const originName = 'next'
+// const originName = 'next'
 
-// not care
-const compressPkgName = `${originName}.tar.gz`
-const user = 'root'
-const origin = 'sugarat.top'
-const fullOrigin = `${originName}.${origin}`
-const baseServerDir = '/www/wwwroot'
-const destDir = ''
+// // not care
+// const compressPkgName = `${originName}.tar.gz`
+// const user = 'root'
+// const origin = 'sugarat.top'
+// const fullOrigin = `${originName}.${origin}`
+// const baseServerDir = '/www/wwwroot'
+// const destDir = ''
 
-await $`pnpm build`
+// await $`pnpm build`
 
-await $`echo ==🔧 压缩dist ==`
-await $`cd packages/blogpress/.vitepress && tar -zvcf ${compressPkgName} dist && rm -rf dist && mv ${compressPkgName} ./../../../`
+// await $`echo ==🔧 压缩dist ==`
+// await $`cd packages/blogpress/.vitepress && tar -zvcf ${compressPkgName} dist && rm -rf dist && mv ${compressPkgName} ./../../../`
 
-await $`echo ==🚀 上传到服务器 ==`
-await $`scp ${compressPkgName} ${user}@${fullOrigin}:./`
-// await $`scp ${compressPkgName} ${user}@ba.sugarat.top:./`
-await $`rm -rf ${compressPkgName}`
+// await $`echo ==🚀 上传到服务器 ==`
+// await $`scp ${compressPkgName} ${user}@${fullOrigin}:./`
+// // await $`scp ${compressPkgName} ${user}@ba.sugarat.top:./`
+// await $`rm -rf ${compressPkgName}`
 
-await $`echo ==✅ 部署代码 ==`
-await $`ssh -p22 ${user}@${fullOrigin} "tar -xf ${compressPkgName} -C ${baseServerDir}/${fullOrigin}/${destDir}"`
-// await $`ssh -p22 ${user}@ba.sugarat.top "tar -xf ${compressPkgName} -C ${baseServerDir}/sugarat.top/${destDir}"`
-// await $`ssh -p22 ${user}@${origin} "tar -xf ${compressPkgName} -C ${baseServerDir}/blog.${origin}/${destDir}"`
+// await $`echo ==✅ 部署代码 ==`
+// await $`ssh -p22 ${user}@${fullOrigin} "tar -xf ${compressPkgName} -C ${baseServerDir}/${fullOrigin}/${destDir}"`
+// // await $`ssh -p22 ${user}@ba.sugarat.top "tar -xf ${compressPkgName} -C ${baseServerDir}/sugarat.top/${destDir}"`
+// // await $`ssh -p22 ${user}@${origin} "tar -xf ${compressPkgName} -C ${baseServerDir}/blog.${origin}/${destDir}"`
