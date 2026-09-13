23fe96c4919e800e90d82f0ec7e93ce24d12087b
sugar <engineerzjl@foxmail.com>
2026-09-13 14:50:05 +0800

    build(blogpress): 添加谷歌广告账号的meta标签

--- packages/blogpress/.vitepress/config.ts
+++ packages/blogpress/.vitepress/config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
   description:
     '粥里有勺糖的个人博客，记录随笔与学习笔记，大前端相关的知识，高频面试题，个人面经等',
   head: [
+    ['meta', { name: 'google-adsense-account', content: 'ca-pub-4256464993013908' }],
     // <meta name="baidu-site-verification" content="codeva-b08avp82Uj" />
     ['meta', { name: 'baidu-site-verification', content: 'codeva-b08avp82Uj' }],
     ['meta', { name: 'theme-color', content: '#ffffff' }],
